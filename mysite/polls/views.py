d3542a68d5ba6fb938ea38178b6155ca3f2249c8
YANAGISAWA Chihiro <chihiroyn@gmail.com>
2018-11-18 10:36:18 +0900

    チュートリアル4の「汎用ビューを使う……」の途中。IndexViewクラスを利用した一覧ページの実装まで。

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,14 +5,23 @@ from django.template import loader
 '''
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+'''
 '''
     template = loader.get_template('polls/index.html')
     context = {
